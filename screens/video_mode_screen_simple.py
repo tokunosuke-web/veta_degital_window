c8aa701329f9d0f86f424314820f4c9aa58282af
tokunosuke-web <takahashitokuno@toki.waseda.jp>
2025-12-03 11:34:25 +0900

    Modify date and time surface positions
    Adjusted the position of date and time surfaces on the screen.

--- screens/video_mode_screen_simple.py
+++ screens/video_mode_screen_simple.py
@@ -156,8 +156,8 @@ class VideoModeScreenPygame:
         date_surface = self.date_font.render(current_date, True, (255, 255, 255))
         time_surface = self.time_font.render(current_time, True, (255, 255, 255))
 
-        self.screen.blit(date_surface, (self.screen_size[0] // 7.5, 1200))
-        self.screen.blit(time_surface, (self.screen_size[0] // 7.5, 1300))
+        self.screen.blit(date_surface, (self.screen_size[0] // 9.5, 1100))
+        self.screen.blit(time_surface, (self.screen_size[0] // 9.5, 1200))
 
     def correct_rotation(self, frame):
         rotation = self.rotation_needed
